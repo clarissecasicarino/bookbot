cfb35d82ac3379ba825f64d732604e0bfc7682ed
clarissecasicarino <cclarissecasi@gmail.com>
2024-03-08 18:02:36 +0800

    Finish print report exercise

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     text = get_book_text(book_path)
     length_of_words = count_words(text)
     counted_letters = count_letters(text)
-    print(f"Letters: {counted_letters}")
+    printed_report = print_report(counted_letters, length_of_words)    
     
 def get_book_text(path):
     with open(path) as f:
@@ -28,7 +28,21 @@ def count_letters(characters):
                 letter_counts[char] = 1
     
     return letter_counts
-            
+
+
+def print_report(counted_letters, word_count):
+    # Convert dictionary to list of dictionaries
+    letter_list = [{'letter': k, 'count': v} for k, v in counted_letters.items()]
+    
+    # Sort the list by the number of occurrences using the provided sort method
+    letter_list.sort(reverse=True, key=lambda x: x['count'])
+    
+    # Printing the report
+    print(f"--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
+    for item in letter_list:
+        print(f"The '{item['letter']}' character was found {item['count']} times")
+    print(f"--- End report ---")       
     
 main()
     
