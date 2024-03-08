72f66cc4fcc21bfd78ed4b07bbbe1e9d332292b5
clarissecasicarino <cclarissecasi@gmail.com>
2024-03-08 17:36:24 +0800

    Finish count_letters exercise

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     length_of_words = count_words(text)
-    print(f"Length of Words: {length_of_words}")
+    counted_letters = count_letters(text)
+    print(f"Letters: {counted_letters}")
     
 def get_book_text(path):
     with open(path) as f:
@@ -11,6 +12,23 @@ def get_book_text(path):
 def count_words(text):
    splitted_text = text.split()
    return len(splitted_text)
+
+def count_letters(characters):
+    letter_counts = {}
+    
+    for char in characters:
+        
+        char = char.lower()
+        
+        # Checks if character is within the alphabet (a-z)
+        if char.isalpha():
+            if char in letter_counts:
+                letter_counts[char] += 1
+            else:
+                letter_counts[char] = 1
+    
+    return letter_counts
+            
     
 main()
     
